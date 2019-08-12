c36961c30ff2dc6072e0f8830af9c4e83845d0e5
CodecommitUser <prasad.kgv@cognizant.com>
2019-08-12 12:17:31 +0000

    removed some files

--- restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo cp openmrs-core/webapp/target/openmrs.war /usr/local/apache-tomcat-7.0.96/webapps/
-sudo cp openmrs-module-legacyui/omod/target/legacyui-1.6.0-SNAPSHOT.omod /root/.OpenMRS/modules/
-sudo /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
