59d15ea1a60b8a8bd04d0dcbae84e3cfe5b2889f
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-12 09:13:10 +0000

    Added restart.sh

---
+++ openmrs-module-legacyui/restart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp /root/awscoe/openmrs-module-legacyui/omod/target/legacyui-1.6.0-SNAPSHOT.omod /root/.OpenMRS/modules/
+sudo /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
