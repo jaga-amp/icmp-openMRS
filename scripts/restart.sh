85ea8887715f20b77ff659e7de408d77df8f08d3
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-12 18:05:26 +0000

    Edited restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-sudo cp openmrs-core/webapp/target/openmrs.war /usr/local/apache-tomcat-7.0.96/webapps/
-sudo cp openmrs-module-legacyui/omod/target/legacyui-1.6.0-SNAPSHOT.omod /root/.OpenMRS/modules/
+#!/bin/sh
+sudo rm -f /usr/local/apache-tomcat-7.0.96/webapps/openmrs.war
 sudo /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
