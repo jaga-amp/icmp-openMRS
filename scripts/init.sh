03f89b52da9d9a3c0d0b91036133a3d4676b2b05
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 13:25:50 +0000

    Edited init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,6 @@
-apt-get update -y
-apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
-wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz
-wget -P /tmp/ https://petclinicwarfiles.s3.amazonaws.com/openmrs-3tireapp/OpenMRS.tar.gz
-tar xvf /tmp/OpenMRS.tar.gz -C /root/
-rm -rf /root/.OpenMRS/modules/legacyui-1.6.0-SNAPSHOT.omod
-tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/
+apt-get update -y 
+apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y 
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
+wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz 
+wget -P /tmp/ https://petclinicwarfiles.s3.amazonaws.com/openmrs-3tireapp/OpenMRS.tar.gz 
+tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/ 
