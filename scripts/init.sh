f036877a7eace09b8363c8c2d11269f0e8b17f3e
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 07:08:24 +0000

    Edited init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,3 @@
-#!/bin/sh 
 sudo apt-get update -y 
 sudo apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y 
 sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
