f57b688ebb8c24c62b4332b8912e7fbbe43f7091
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 07:09:37 +0000

    Edited restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
-sudo rm -f /usr/local/apache-tomcat-7.0.96/webapps/openmrs.war
+sudo rm -rf /usr/local/apache-tomcat-7.0.96/webapps/openmrs*
 sudo /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
