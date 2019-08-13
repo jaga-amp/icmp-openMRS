dce41c331181afed39082fe51a90e57f2ac98cb8
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 13:27:02 +0000

    Edited restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,2 +1,4 @@
+tar xvf /tmp/OpenMRS.tar.gz -C /root/ 
+rm -rf /root/.OpenMRS/modules/legacyui-1.6.0-SNAPSHOT.omod 
 rm -rf /usr/local/apache-tomcat-7.0.96/webapps/openmrs*
 /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
