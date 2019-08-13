f62cb043473e347fd4c2233fb055046a73e88b71
CodecommitUser <prasad.kgv@cognizant.com>
2019-08-13 13:35:27 +0000

    modified files

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
-apt-get update -y 
-apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y 
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
-wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz 
-wget -P /tmp/ https://petclinicwarfiles.s3.amazonaws.com/openmrs-3tireapp/OpenMRS.tar.gz 
-tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/ 
+apt-get update -y
+apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz
+wget -P /tmp/ https://petclinicwarfiles.s3.amazonaws.com/openmrs-3tireapp/OpenMRS.tar.gz
+tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/
+
