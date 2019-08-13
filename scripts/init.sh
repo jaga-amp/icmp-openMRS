64ab9bfc8617da77f70bff9d10322f4f895f07cd
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 16:33:34 +0000

    Edited init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,9 @@ apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
 wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz 
 wget -P /tmp/ https://petclinicwarfiles.s3.amazonaws.com/openmrs-3tireapp/OpenMRS.tar.gz 
+mkdir /root/awscoe/
 tar xvf /tmp/OpenMRS.tar.gz
 mv /tmp/.OpenMRS /root/
 rm -rf /root/.OpenMRS/modules/legacyui-1.6.0-SNAPSHOT.omod 
+cp -r /root/awscoe/legacyui-1.6.0-SNAPSHOT.omod /root/.OpenMRS/modules/
 tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/ 
