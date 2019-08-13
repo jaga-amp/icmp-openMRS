a27f18c12c525728b8a324169cf6ca7332bb891f
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-13 15:58:49 +0000

    Edited restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,2 +1,7 @@
+cp -r /tmp/.OpenMRS/owa /root/.OpenMRS/
+cp -r /tmp/.OpenMRS/openmrs.log /root/.OpenMRS/
+cp -r /tmp/.OpenMRS/openmrs-runtime.properties /root/.OpenMRS/
+cp -r /tmp/.OpenMRS/lucene /root/.OpenMRS/
+cp -r /tmp/.OpenMRS/liquibaseUpdateLogs.txt /root/.OpenMRS/
 rm -rf /usr/local/apache-tomcat-7.0.96/webapps/openmrs*
 /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
