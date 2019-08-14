14c969d59b0efb9d18dd397153aa8d034520114b
CodecommitUser <prasad.kgv@cognizant.com>
2019-08-14 02:44:19 +0000

    modified some files

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,2 @@
-tar xvf /tmp/OpenMRS.tar.gz
-cp -r /tmp/.OpenMRS /root/
-rm -rf /root/.OpenMRS/modules/legacyui-1.6.0-SNAPSHOT.omod 
-cp -r /root/awscoe/legacyui-1.6.0-SNAPSHOT.omod /root/.OpenMRS/modules/
 rm -rf /usr/local/apache-tomcat-7.0.96/webapps/openmrs*
 /usr/local/apache-tomcat-7.0.96/bin/catalina.sh start
