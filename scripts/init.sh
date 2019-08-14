26e70d87ab24d9d49f22bfa35d21f91fb94279be
openmrs <CISTOAWSCOE@cognizant.com>
2019-08-14 02:50:38 +0000

    Edited init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
-apt-get update -y 
-apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y 
-update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
-wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz 
-tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/ 
+sudo apt-get update -y 
+sudo apt install openjdk-8-jdk openjdk-8-jre maven git python3-pip -y 
+sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 
+sudo wget -P /tmp/ http://mirrors.ocf.berkeley.edu/apache/tomcat/tomcat-7/v7.0.96/bin/apache-tomcat-7.0.96.tar.gz 
+sudo tar xvf /tmp/apache-tomcat-7.0.96.tar.gz -C /usr/local/ 
